fb6d46ca23de05e0f1f7b1d88a8656b7bcd5f86f
Olaf Alders <olaf@wundersolutions.com>
2015-07-28 09:40:04 -0400

    Adds examples for hosted-form-url and capture-delayed-transaction.

---
+++ examples/hosted-form-url.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/env perl;
+
+use strict;
+use warnings;
+use feature qw( say );
+
+use lib 't/lib';
+use Util;
+
+my $payments = Util::ppa();
+
+my $response = $payments->create_secure_token(
+    {
+        AMT            => 100,
+        TRXTYPE        => 'S',
+        VERBOSITY      => 'HIGH',
+        BILLINGTYPE    => 'MerchantInitiatedBilling',
+        CANCELURL      => 'https://example.com/cancel',
+        ERRORURL       => 'https://example.com/error',
+        L_BILLINGTYPE0 => 'MerchantInitiatedBilling',
+        NAME           => 'Chuck Norris',
+        RETURNURL      => 'https://example.com/return',
+    }
+);
+
+my $uri      = $payments->hosted_form_uri($response);
+say $uri;
